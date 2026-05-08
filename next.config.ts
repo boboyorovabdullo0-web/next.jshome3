52a2261c301c896a26a2b067e23c62a894ba8f69
Boboyorov Abdullo <Boboyorovabdullo0@gmail.com>
2026-05-08 15:23:02 +0500

    first commit

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+// next.config.ts
+import { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const nextConfig: NextConfig = {};
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+export default withNextIntl(nextConfig);
